4add4ea17f47c122461f62e134ca70eab198571e
猪圈 <ethanote@gmail.com>
2018-10-10 00:40:48 +0800

    支持协程，并发下载文件

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+import gevent
+from gevent import getcurrent
+from gevent import monkey; monkey.patch_all()
+from gevent.pool import Pool
+from multiprocessing import cpu_count
 from qiniu import Auth
 from qiniu import BucketManager
 import utils
 import requests
 import os
+import time;
 
 def listfiles(bucket, bucket_name, prefix=None, limit=None):
 	if bucket is None:
@@ -19,6 +25,24 @@ def listfiles(bucket, bucket_name, prefix=None, limit=None):
 		raise Exception("list file Error!")
 	return items
 
+def downloadfile(index, private_url, file_name):
+	time_start = time.time()
+	result = False;
+	try:
+		r = requests.get(private_url)
+		if r.status_code == 200:
+			file = open(file_name, "wb")
+			file.write(r.content)
+			file.flush()
+			file.close()
+			result = True
+	except Exception as e:
+		pass
+	time_end = time.time()
+	elapsed = time_end - time_start
+	utils.record_log('download: (%s) -> %s, result: %s, time: %ss' % (index, private_url, result, elapsed))
+	return result, index, private_url
+
 if __name__ == '__main__':	
 	access_key = ''
 	secret_key = ''
@@ -30,24 +54,30 @@ if __name__ == '__main__':
 	q = Auth(access_key, secret_key)
 	bucket = BucketManager(q)
 	items = listfiles(bucket, bucket_name)
-	utils.record_log('total files: ' + str(len(items)))
+	utils.record_log('total files: %s' % len(items))
+	results = []
+	jobs = []
+	p = Pool(cpu_count() * 2)
+	time_start = time.time()
 	for index, item in enumerate(items):
 		key = item['key']
-		# print(key)
+		if '/' in key:
+			key_path = path + key[:key.rfind('/')]
+			if not os.path.exists(key_path):
+				os.makedirs(key_path)
 		base_url = 'http://%(domain)s/%(key)s' % {"domain": bucket_domain, "key": key}
-		# print(base_url)
 		private_url = q.private_download_url(base_url, expires=3600)
-		# print(private_url)
-		r = requests.get(private_url)
-		if r.status_code == 200:
-			if '/' in key:
-				key_path = path + key[:key.rfind('/')]
-				if not os.path.exists(key_path):
-					os.makedirs(key_path)
-			file = open(path + key, "wb")
-			file.write(r.content)
-			file.flush()
-			file.close()
-			utils.record_log(str(index) + ": " + private_url + " download success")
+		jobs.append(p.spawn(downloadfile, index, private_url, path + key))
+	results = gevent.joinall(jobs)
+	time_end = time.time()
+	elapsed = time_end - time_start
+	utils.record_log('finished: %ss' % elapsed)
+	successful = 0
+	unsuccessful = 0
+	for result in results:
+		if result.successful() and result.value[0] is True:
+			successful+=1
 		else:
-			utils.record_log(private_url + " download failed")
+			unsuccessful+=1
+			lists.append('%s -> %s' % (result.value[1], result.value[2]))
+	utils.record_log('total: %s, success: %s, unsuccessful: %s' % (len(jobs), successful, unsuccessful))
